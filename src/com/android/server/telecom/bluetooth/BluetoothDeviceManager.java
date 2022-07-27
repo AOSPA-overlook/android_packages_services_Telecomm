038bb40291912cefd40dce865214886c64e21962
Muhammed Siju <quic_c_msiju@quicinc.com>
2022-07-27 18:58:04 +0530

    Fix call audio switch issue for BT LE to speaker and back to BT switch
    Currently when call audio is switched from BT LE headset to speaker,
    first audio communication device is set as speaker and then BT audio
    is disconnected. Hence onAudioLost() is not invoked when
    clearLeAudioCommunicationDevice() is called. This causes
    BluetoothRouteManager to wrongly assume that the BT LE headset
    is in AudioConnected state. Hence when audio is switched back to BT,
    BluetoothRouteManager doesn't try to connect BT audio and switch
    fails. Fix this by making sure that onAudioLost() is always
    invoked when clearLeAudioCommunicationDevice() is called.
    
    Change-Id: I7b8a8545d896e6be12ffbbdc690e9e83c050e56d
    CRs-Fixed: 3246566

--- src/com/android/server/telecom/bluetooth/BluetoothDeviceManager.java
+++ src/com/android/server/telecom/bluetooth/BluetoothDeviceManager.java
@@ -186,6 +186,7 @@ public class BluetoothDeviceManager {
     private boolean mLeAudioCallbackRegistered = false;
     private BluetoothLeAudio mBluetoothLeAudioService;
     private boolean mLeAudioSetAsCommunicationDevice = false;
+    private String mLeAudioDevice;
     private boolean mHearingAidSetAsCommunicationDevice = false;
     private BluetoothDevice mBluetoothHearingAidActiveDeviceCache;
     private BluetoothAdapter mBluetoothAdapter;
@@ -422,11 +423,15 @@ public class BluetoothDeviceManager {
 
     public void clearLeAudioCommunicationDevice() {
         Log.i(this, "clearLeAudioCommunicationDevice: mLeAudioSetAsCommunicationDevice = " +
-                mLeAudioSetAsCommunicationDevice);
+                mLeAudioSetAsCommunicationDevice + " device = " + mLeAudioDevice);
         if (!mLeAudioSetAsCommunicationDevice) {
             return;
         }
         mLeAudioSetAsCommunicationDevice = false;
+        if (mLeAudioDevice != null) {
+            mBluetoothRouteManager.onAudioLost(mLeAudioDevice);
+            mLeAudioDevice = null;
+        }
 
         if (mAudioManager == null) {
             Log.i(this, "clearLeAudioCommunicationDevice: mAudioManager is null");
@@ -435,7 +440,6 @@ public class BluetoothDeviceManager {
         if (mAudioManager.getCommunicationDevice() != null
                 && mAudioManager.getCommunicationDevice().getType()
                 == AudioDeviceInfo.TYPE_BLE_HEADSET) {
-            mBluetoothRouteManager.onAudioLost(mAudioManager.getCommunicationDevice().getAddress());
             mAudioManager.clearCommunicationDevice();
         }
     }
@@ -503,6 +507,7 @@ public class BluetoothDeviceManager {
             Log.i(this, " bleHeadset device set");
             mBluetoothRouteManager.onAudioOn(bleHeadset.getAddress());
             mLeAudioSetAsCommunicationDevice = true;
+            mLeAudioDevice = bleHeadset.getAddress();
         }
         return result;
     }
